6629cd7e59a658defd167b70ca88e700aa50cb17
Buğrahan Kök <bugrahankok@gmail.com>
2024-01-27 03:41:17 +0100

    Rename database file

--- ParkingLotManagement/Helpers/DatabaseUtils.cs
+++ ParkingLotManagement/Helpers/DatabaseUtils.cs
@@ -2,7 +2,7 @@
 {
     internal static class DatabaseUtils
     {
-        public static string DATABASE_PATH = "parklotdata.db";
+        public static string DATABASE_PATH = "CarPark.db";
         public static string CONNECTION_STRING = $"Data Source={DATABASE_PATH};Version=3;";
     }
 }
